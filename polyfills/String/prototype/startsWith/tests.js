083d4dc57d2f809993ed1cdcabbab33e7a996ec0
Jake Champion <jakechampion.jake2@googlemail.com>
2018-02-01 00:54:24 +0000

    imrpove test coverage

--- polyfills/String/prototype/startsWith/tests.js
+++ polyfills/String/prototype/startsWith/tests.js
@@ -1,8 +1,76 @@
 /* eslint-env mocha, browser */
 /* global proclaim */
 
-it('has correct instance', function () {
-	proclaim.isInstanceOf(String.prototype.startsWith, Function);
+it('is a function', function () {
+	proclaim.isFunction(String.prototype.startsWith);
 });
 
-// TODO: Add more tests.
+it('has correct argument length', function () {
+	proclaim.strictEqual(String.prototype.startsWith.length, 1);
+});
+
+it('has correct name', function () {
+	var functionsHaveNames = (function foo() {}).name === 'foo';
+	if (functionsHaveNames) {
+		proclaim.equal(String.prototype.startsWith.name, 'startsWith');
+	} else {
+		function nameOf(fn) {
+			return Function.prototype.toString.call(fn).match(/function\s*([^\s]*)\s*\(/)[1];
+		}
+		proclaim.equal(nameOf(String.prototype.startsWith), 'startsWith');
+	}
+});
+
+var arePropertyDescriptorsSupported = function () {
+	var obj = {};
+	try {
+		Object.defineProperty(obj, 'x', {
+			enumerable: false,
+			value: obj
+		});
+		/* eslint-disable no-unused-vars, no-restricted-syntax */
+		for (var _ in obj) {
+			return false;
+		}
+		/* eslint-enable no-unused-vars, no-restricted-syntax */
+		return obj.x === obj;
+	} catch (e) { // this is IE 8.
+		return false;
+	}
+};
+var ifSupportsDescriptors = Object.defineProperty && arePropertyDescriptorsSupported() ? it : xit;
+
+ifSupportsDescriptors('property is not enumerable', function () {
+	proclaim.isFalse(Object.prototype.propertyIsEnumerable.call(String.prototype.startsWith));
+});
+
+it('works as expected', function () {
+	proclaim.ok('undefined'.startsWith());
+	proclaim.ok(!'undefined'.startsWith(null));
+	proclaim.ok('abc'.startsWith(''));
+	proclaim.ok('abc'.startsWith('a'));
+	proclaim.ok('abc'.startsWith('ab'));
+	proclaim.ok(!'abc'.startsWith('bc'));
+	proclaim.ok('abc'.startsWith('', NaN));
+	proclaim.ok('abc'.startsWith('a', -1));
+	proclaim.ok(!'abc'.startsWith('a', 1));
+	proclaim.ok(!'abc'.startsWith('a', Infinity));
+	proclaim.ok('abc'.startsWith('b', true));
+	proclaim.ok('abc'.startsWith('a', 'x'));
+	proclaim.throws(function(){
+		String.prototype.startsWith.call(null, '.');
+	}, TypeError);
+	proclaim.throws(function(){
+		String.prototype.startsWith.call(undefined, '.');
+	}, TypeError);
+	proclaim.throws(function(){
+		'/./'.startsWith(/./);
+	}, TypeError);
+	proclaim.ok((function(){
+		try {
+			return '[object Object]'.startsWith({});
+		} catch (e) {
+			return false;
+		}
+	}()));
+});
