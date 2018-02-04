33ad07f8481638bbc67f5858bf42e4c5389c6098
Jake Champion <jakechampion.jake2@googlemail.com>
2018-02-04 16:19:39 +0000

    Object.getOwnPropertyDescriptor tests

--- polyfills/Object/getOwnPropertyDescriptor/tests.js
+++ polyfills/Object/getOwnPropertyDescriptor/tests.js
@@ -1,5 +1,106 @@
-/* eslint-env mocha, browser */
-/* global proclaim */
+/* eslint-env mocha */
+/* globals proclaim */
+
+proclaim.arity = function (fn, expected) {
+	this.isFunction(fn);
+	this.strictEqual(fn.length, expected);
+};
+proclaim.name = function (fn, expected) {
+	var functionsHaveNames = (function foo() { }).name === 'foo';
+	if (functionsHaveNames) {
+		this.strictEqual(fn.name, expected);
+	} else {
+		this.equal(Function.prototype.toString.call(fn).match(/function\s*([^\s]*)\s*\(/)[1], expected);
+	}
+};
+proclaim.nonEnumerable = function (obj, prop) {
+	var arePropertyDescriptorsSupported = function () {
+		var obj = {};
+		try {
+			Object.defineProperty(obj, 'x', { enumerable: false, value: obj });
+					/* eslint-disable no-unused-vars, no-restricted-syntax */
+					for (var _ in obj) { return false; }
+					/* eslint-enable no-unused-vars, no-restricted-syntax */
+			return obj.x === obj;
+		} catch (e) { // this is IE 8.
+			return false;
+		}
+	};
+	if (Object.defineProperty && arePropertyDescriptorsSupported()) {
+		this.isFalse(Object.prototype.propertyIsEnumerable.call(obj[prop]));
+	}
+};
+
+var propertyDescriptorsSupported = (function () {
+	var obj = {};
+	try {
+		Object.defineProperty(obj, 'x', { enumerable: false, value: obj });
+        /* eslint-disable no-unused-vars, no-restricted-syntax */
+        for (var _ in obj) { return false; }
+        /* eslint-enable no-unused-vars, no-restricted-syntax */
+		return obj.x === obj;
+	} catch (e) { // this is IE 8.
+		return false;
+	}
+}());
+
+it('is a function', function () {
+	proclaim.isFunction(Object.getOwnPropertyDescriptors);
+});
+
+it('has correct arity', function () {
+	proclaim.arity(Object.getOwnPropertyDescriptors.length, 1);
+});
+
+it('has correct name', function() {
+	proclaim.name(Object.getOwnPropertyDescriptors, 'getOwnPropertyDescriptors');
+});
+
+it('is not enumerable', function () {
+	proclaim.nonEnumerable(Object, 'entries');
+});
+
+it('works as expected', function () {
+	var getOwnPropertyDescriptors, O, s, descs;
+	getOwnPropertyDescriptors = Object.getOwnPropertyDescriptors;
+	if ('create' in Object) {
+		O = Object.create({
+			q: 1
+		}, {
+				e: {
+					value: 3
+				}
+			});
+		O.w = 2;
+		s = Symbol('s');
+		O[s] = 4;
+		descs = getOwnPropertyDescriptors(O);
+		proclaim.strictEqual(descs.q, void 8);
+		proclaim.deepEqual(descs.w, {
+			enumerable: true,
+			configurable: true,
+			writable: true,
+			value: 2
+		});
+		if (propertyDescriptorsSupported) {
+			proclaim.deepEqual(descs.e, {
+				enumerable: false,
+				configurable: false,
+				writable: false,
+				value: 3
+			});
+		} else {
+			proclaim.deepEqual(descs.e, {
+				enumerable: true,
+				configurable: true,
+				writable: true,
+				value: 3
+			});
+		}
+		proclaim.strictEqual(descs[s].value, 4);
+	}
+});
+
 
 // Copied from ES5-Shim
 
